862d835f28099917194c3c2fb180c7b1589a6346
Khaliffa Regian Gunawan <krg180701@gmail.com>
2024-05-28 16:40:05 +0700

    layouts

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="iq-sidebar">
             <div class="iq-sidebar-logo d-flex justify-content-between">
                <a href="index.html">
-               <img src="images/logo.png" class="img-fluid" alt="">
-               <span>Sofbox</span>
+               <img src="images/iconbaru.jpeg" class="img-fluid" alt="">
+               <span>IoT1</span>
                </a>
                <div class="iq-menu-bt align-self-center">
                   <div class="wrapper-menu">
